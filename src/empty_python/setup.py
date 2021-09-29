aedabe73c4ce32d7088deb20829cc48b368d5f58
tlips <thomas.lips@ugent.be>
2021-09-29 19:57:32 +0200

    annotate build config

--- src/empty_python/setup.py
+++ src/empty_python/setup.py
@@ -6,7 +6,7 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
+    data_files=[ # "mount files"
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'empty_python_node = empty_python.empty_python_node:main'
+            'empty_python_node = empty_python.empty_python_node:main' # declare 'executable' <name> <package>.<file>:<entry-function>
         ],
     },
 )
