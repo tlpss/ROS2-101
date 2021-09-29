b2bf12819a24e47687435153dd4e0a143048af89
tlips <thomas.lips@ugent.be>
2021-09-29 21:33:52 +0200

    make python empty package pass tests

--- src/empty_python/setup.py
+++ src/empty_python/setup.py
@@ -6,7 +6,7 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[ # "mount files"
+    data_files=[  # "mount files"
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'empty_python_node = empty_python.empty_python_node:main' # declare 'executable' <name> <package>.<file>:<entry-function>
+            # declare 'executable' <name> <package>.<file>:<entry-function>
+            'empty_python_node = empty_python.empty_python_node:main'
         ],
     },
 )
