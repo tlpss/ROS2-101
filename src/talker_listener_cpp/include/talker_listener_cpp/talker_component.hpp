3960faf149ca6bcf9a5719339a3fb587ed73b531
tlips <thomas.lips@ugent.be>
2021-10-04 17:51:22 +0200

    create header (required for component)

---
+++ src/talker_listener_cpp/include/talker_listener_cpp/talker_component.hpp
@@ -0,0 +1,26 @@
+#ifndef COMPOSITION__TALKER_COMPONENT_HPP_
+#define COMPOSITION__TALKER_COMPONENT_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+namespace composition
+{
+
+class Talker : public rclcpp::Node
+{
+public:
+  explicit Talker(const rclcpp::NodeOptions & options);
+
+protected:
+  void on_timer();
+
+private:
+  size_t count_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+}  // namespace composition
+
+#endif  // COMPOSITION__TALKER_COMPONENT_HPP_
