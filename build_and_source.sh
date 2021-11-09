73d57b7a1d9b02e0be4daec8cf8b66e2f0cc09d3
tlips <thomas.lips@ugent.be>
2021-11-09 21:30:45 +0000

    setup dev container

--- build_and_source.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/shell
-source /opt/ros/foxy/setup.bash
-colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --symlink-install  # symlink to make python scripts changeable & compile commands export for intellisense
-source install/setup.bash
